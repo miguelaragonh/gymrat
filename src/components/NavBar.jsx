611c93bde866ed8f1c3d78851c8371024ea8261e
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-03-11 00:27:51 -0600

    nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,13 @@ export default function NavBar(){
         <>
         <div class="tab-container">
             <input type="radio" name="tab" id="tab1" class="tab tab--1" />
-            <label class="tab_label" for="tab1">Profile</label>
+            <label class="tab_label" for="tab1">Inicio</label>
 
             <input type="radio" name="tab" id="tab2" class="tab tab--2" />
-            <label class="tab_label" for="tab2">Settings</label>
+            <label class="tab_label" for="tab2">Ejercicios</label>
 
             <input type="radio" name="tab" id="tab3" class="tab tab--3" />
-            <label class="tab_label" for="tab3">Notifications</label>
+            <label class="tab_label" for="tab3">Contarnos</label>
 
             <div class="indicator"></div>
         </div>
